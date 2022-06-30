14e1363c6af5444c61c19810a0754463667f6722
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2022-06-30 18:31:22 -0300

    Modificações gerais
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import QRCode from "react-qr-code";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const [value, setValue] = useState("");
+  const [url, setUrl] = useState(0);
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    let currentUrl = `${window.location.href}api/mercado?price=${value}`;
+    setUrl(currentUrl);
+  });
 
   return (
-    <main>
+    <main className={styles.main}>
       <h2>Valor:</h2>
-      <input type="text" />
+      <input
+        type="number"
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
 
       <h2>QR Code:</h2>
-      <QRCode value="hey" />
+      <QRCode value={url} />
+      <p>{url}</p>
     </main>
   );
 }
